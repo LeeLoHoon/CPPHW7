d8caca06d3b1b8c7e9b558e8e38de6e7508d2858
leelohoon <49421109+LeeLoHoon@users.noreply.github.com>
2019-05-13 01:22:06 +0900

    Update BST.h

--- BST.h
+++ BST.h
@@ -22,10 +22,12 @@ class BST{
     //return 받는 tree를 parent로 받고 Add함수에서 parent보다 작으면 왼쪽, 크면 오른쪽에 insert
     //2. Add함수 콜 ==> 1)root가 NULL인 상태이면 들어오는 key와 value를 root의 num과 str에 넣음 2)root가 NULL상태가 아니면 위에 알고리즘 사용
     //3. Delete 경우의 수 3가지 reaf==> 그냥 지움,reaf아니지만 자손 하나 ptr다음꺼로 바꾸고 지움  ,reaf 아니고 자손 둘 ==> 그 오른쪽 subtree에서 제일 작은 것을 지울 tree에 넣고 지움
+    tree_node* search(tree_node *tree,int key);
     tree_node* search(int key);
-    tree_node* getRoot();
-    void setRoot(int key, string value);
-    void Add(int key, string value);
+    tree_node* Add(tree_node *tree,int key, string value);
+    void Add(int key, string value){
+      root=Add(root,key,value);
+    }
     void Delete(int key);
     void Print();
     //inorder로 print하기 key랑 value. A+B 순
