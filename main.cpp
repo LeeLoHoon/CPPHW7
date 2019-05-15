d103211465d9debc788f94305fed850fa8e8eb8e
leelohoon <49421109+LeeLoHoon@users.noreply.github.com>
2019-05-15 17:45:15 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include <fstream>
 #include <string>
 
-//command [key[value]] 순으로 받는데 if 문써서 하나씩 입력받음 받은게 a라면 key와 value를 순서대로 받고 d를 받으면 key만 받음 p를 받으면 더 받지 않고 모두 프린트하기 q를 받으면 프로그램 종료 ==>get함수에 넣기
-//BST에 구현되어야 할 함수 : search 함수, add 함수, delete함수, print함수, get함수
 using namespace std;
 
 int main() {
